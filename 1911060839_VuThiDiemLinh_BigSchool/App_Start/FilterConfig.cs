20f5fb3458f5a6230b16cb5db84c1677238c153a
Diem Linh <Diem Linh@DESKTOP-08N4DNV>
2022-03-21 08:58:55 +0700

    Add project files.

---
+++ 1911060839_VuThiDiemLinh_BigSchool/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _1911060839_VuThiDiemLinh_BigSchool
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
